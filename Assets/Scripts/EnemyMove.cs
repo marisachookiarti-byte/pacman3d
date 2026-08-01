3073abda6935f396c6eb4c8c8d30e9c3a4b2e4f5
Ken <38210245+dogchydios@users.noreply.github.com>
2026-08-01 23:34:37 +0700

    -fixed cat dying. -fixed cat colliding with coin. -used async for cat timing.

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -1,19 +1,45 @@
+using System;
 using Pacman;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class EnemyMove : MonoBehaviour
 {
+    public bool isEnabled = true;
+    
     public NavMeshAgent cat;
     private NavMeshAgent _player;
+    private Health _catHealth;
+
+    void Awake()
+    {
+        _catHealth = GetComponent<Health>();
+        _catHealth.onDeath.AddListener(onDeath);
+    }
+    
     void Start()
     {
         cat.speed = Random.Range(1,3);
         
         _player = GameController.Instance.pac.GetComponentInChildren<NavMeshAgent>();
     }
+
+    private void OnDestroy()
+    {
+        _catHealth.onDeath.RemoveListener(onDeath);
+    }
+
+    void onDeath()
+    {
+        isEnabled = false;
+        cat.enabled = false;
+    }
+    
     void Update()
     {
+        if (!isEnabled) return;
+        
         if (cat.isOnNavMesh)
         {
             cat.SetDestination(_player.transform.position);
@@ -26,4 +52,6 @@ public class EnemyMove : MonoBehaviour
             }
         }
     }
+    
+    
 }
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
@@ -10,28 +12,62 @@ public class Health : MonoBehaviour
 
     public Material skin;
     public Rigidbody rb;
+    public Collider attackCollider;
+
+    public UnityEvent onDeath;
 
     void Awake()
     {
-        //skin = new Material(skin);
+        if (skin != null)
+        {
+            Material sharedSkin = skin;
+            skin = new Material(sharedSkin);
+
+            foreach (Renderer renderer in GetComponentsInChildren<Renderer>(true))
+            {
+                Material[] materials = renderer.sharedMaterials;
+                for (int i = 0; i < materials.Length; i++)
+                {
+                    if (materials[i] == sharedSkin)
+                        materials[i] = skin;
+                }
+                renderer.sharedMaterials = materials;
+            }
+        }
         sfxSource.mute = false;
+        
+        attackCollider.GetComponent<Collider>();
+            
+        onDeath.AddListener(catFloatOnDeath);
+        onDeath.AddListener(() => colorChange(Color.blueViolet));
+
+    }
+
+    private void OnDestroy()
+    {
+        if (skin != null)
+            Destroy(skin);
     }
     void Update()
     {
         
     }
-    private void OnCollisionEnter(Collision collision)
+    
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.CompareTag("bullet"))
-        {
-            health--;
-            sfxSource.PlayOneShot(meowSound);
-            Destroy(collision.gameObject);
-            checkHealth();
-            colorChange(Color.red);
-            Invoke("colorChange", .1f);
-        }
+        Debug.Log($"OnTriggerEnter object={other.gameObject.name}");
+
+        if (!other.CompareTag("bullet"))
+            return;
+
+        health--;
+        sfxSource.PlayOneShot(meowSound);
+        Destroy(other.gameObject);
+
+        if (checkHealth())
+            catColorBlink(Color.red, 0.1f);
     }
+    
     private void colorChange(Color c)
     {
         skin.color = c;
@@ -40,16 +76,41 @@ public class Health : MonoBehaviour
     {
         skin.color = Color.white;
     }
-    private void checkHealth()
+    
+    private bool checkHealth()
     {
         if (health <= 0)
         {
-            colorChange(Color.blueViolet);
-            while (transform.position.y<5) {
-                Console.Write("floating");
-                transform.position += transform.up*Time.deltaTime;
-            }
-            Destroy(gameObject);
+            onDeath.Invoke();
+            return false;
         }
+
+        return true;
+    }
+
+    private async void catFloatOnDeath()
+    {
+        rb.useGravity = false;
+        
+        
+        float startingY = transform.position.y;
+        float endingY = startingY + 5;
+        //Debug.Log($"catFloatOnDeath startingY={startingY} endingY={endingY}");
+
+        while (transform.position.y < endingY) {
+            //Debug.Log($"catFloatOnDeath whileLoop transform.position.y= {transform.position.y} {transform.position.y < endingY} = transform.position.y < endingY");
+
+            transform.position += transform.up * Time.deltaTime;
+            await Task.Yield();
+        }
+        
+        Destroy(gameObject);
+    }
+    
+    private async void catColorBlink(Color c, float waitTimeSecond)
+    {
+        colorChange(Color.red);
+        await Awaitable.WaitForSecondsAsync(waitTimeSecond);
+        colorChange();
     }
 }
